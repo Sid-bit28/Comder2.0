c75cfd8affae2a9a02b91c2ee5e2dac8202b86ab
Siddarth Banerjee <siddarth2801@gmail.com>
2022-07-15 12:02:45 +0530

    initial commit

--- components/Navbar.js
+++ components/Navbar.js
@@ -5,15 +5,15 @@ export default function Navbar() {
   return (
     <div className="container">
       <nav>
-        <Image src="/rupee.png" width={50} height={48} />
-        <h1>Gaming Vibes</h1>
+        <Image src="/logo.png" width={50} height={48} />
+        <h1>Comder2.0</h1>
         <ul>
           <li><Link href="/"><a>Home</a></Link></li>
-          <li><Link href="/guides"><a>Guides</a></Link></li>
+          <li><Link href="/guides"><a>Problems</a></Link></li>
         </ul>
       </nav>
       <div className="banner">
-        <Image src="/banner.png" width={966} height={276} />
+        <Image src="/banner.jpg" width={966} height={276} />
       </div>
     </div>
   )
